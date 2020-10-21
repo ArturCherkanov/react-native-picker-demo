238f052c306bd7442d8216353a919e0c7ba982f9
lawrence <lawrenceo@activedgetechnologies.com>
2020-10-21 23:04:29 +0100

    setup react-naive-picker

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import RNPickerSelect from "react-native-picker-select";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App () {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style='auto' />
+			<RNPickerSelect
+				items={[
+					{ label: "Football", value: "football" },
+					{ label: "Baseball", value: "baseball" },
+					{ label: "Hockey", value: "hockey" },
+				]}
+			/>
+			<Text>Hello world</Text>
 		</View>
 	);
 }
